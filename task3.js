c279dff452bf9e63397d97026cae4597498b5313
codelady-ship <jalilova.antiqa@gmail.com>
2024-12-06 15:09:26 +0400

    comment

---
+++ task3.js
@@ -0,0 +1,140 @@
+//TASK2. 1-den verilen edede qeder olan ededlerin cemi.
+let n =25;
+let cem=0;
+for(let i=1;i<n;i++){
+    cem+=i
+}
+document.write(`1 den ${n}ededine qeder butun reqemlerin cemi =${cem} <br/>`)
+
+// -------------------------------------------------------------------------------------------------------------------
+/*TASK3. ekranda bu formada sekil cekin
+****
+***
+**
+**/
+/*const a="****"
+const b="***"
+const c="**"
+const d="**"
+for(let i= a.length-1;i>= 0;i--){document.write(a
+    [i])}
+for(let i= b.length-1;i>= 0;i--){document.write(b
+    [i])}
+for(let i= c.length-1;i>= 0;i--){document.write(c
+    [i])}
+for(let i= d.length-1;i>= 0;i--){document.write(d
+    [i])}*/
+
+
+// -------------------------------------------------------------------------------------------------------------------
+
+//TASK4.  1-den verilen edede qeder ededlerin hasili -=>  faktorial (5!= 1*2*3*4*5)
+let a= 25;
+let result =1;
+for(let i=1;i<=a;i++){
+    result*=i;
+   }
+   document.write (`1 den ${a}ededine qeder butun reqemlerin hasili =${result}`)
+
+
+/*Task-01
+İkirəqəmli ədədi üçrəqəmli olana qədər artırın.
+İstifadəçidən ikirəqəmli ədəd daxil etməsini xahiş edin.
+Bu rəqəmi üçrəqəmli olana kimi 7 - ədədi qədər artırın.
+Son dəyəri konsola çıxarın.
+
+Task-02
+Konsola N dəfə «I know how to use cycles» mesajı çıxaran proqram yazın.
+Proqram N ədədini istifadəçidən soruşur.
+
+Task-03
+Sonu 0 ilə bitən bütün üçrəqəmli ədədləri konsola çıxaran proqram yazın.
+
+Task-04
+Bütün ikirəqəmli tək ədədlərin cəmini konsola çıxaran proqram yazın.
+
+Task-05
+İstifadəçinin daxil etdiyi ədədin bölünəni olan bütün üçrəqəmli ədədlərin cəmini hesablayan proqram yazın.
+Proqram istifadəçidən ədəd daxil etməyi xahiş edir. Bundan sonra proqram konsolda bu rəqəmin
+bölünəni olan bütün üçrəqəmli ədədlərin cəmini çıxarır.*/
+
+/*Task-06
+Sadə ədədləri çıxaran proqram yazın.
+İstifadəçidən n ədədini soruşan və n-dən 0-a kimi bütün tek ədədləri azalan ardıcıllıqla çıxaran proqram yazın.
+«16» ədədi daxil edildikdə, konsola «15», «13», «11», «9», «7», «5», «3», «1» ardıcıllığı çıxarılmalıdır.*/
+/*let a=+prompt("eded daxil et")
+for(let i=1 ;i<=a;i++2)
+
+
+/*Task-07-dən Task-10-a qədər for ilə yazılmalıdır!
+Task-07
+10-a qədər bütün ədədlərin hasilin tapan proqram tərtib edin
+For dövrün köməyi ilə 10-a qədər bütün ədədlərin hasilin tapan proqram tərtib edin.//TASK2. 1-den verilen edede qeder olan ededlerin cemi.
+let start =1;
+let end=start++;
+for(let start;start<=5;start++){console.log(end)}
+
+// -------------------------------------------------------------------------------------------------------------------
+/*TASK3. ekranda bu formada sekil cekin
+****
+***
+**
+**/
+/*const a="****"
+const b="***"
+const c="**"
+const d="**"
+for(let i= a.length-1;i>= 0;i--){document.write(a
+    [i])}
+for(let i= b.length-1;i>= 0;i--){document.write(b
+    [i])}
+for(let i= c.length-1;i>= 0;i--){document.write(c
+    [i])}
+for(let i= d.length-1;i>= 0;i--){document.write(d
+    [i])}
+
+
+// -------------------------------------------------------------------------------------------------------------------
+
+// TASK4.  1-den verilen edede qeder ededlerin hasili -=>  faktorial (5!= 1*2*3*4*5)
+let birinci= 1;
+let result *= birinci;
+for(let birinci;birinci<=5;birinci++){document.write(result)}
+
+
+/*Task-01
+İkirəqəmli ədədi üçrəqəmli olana qədər artırın.
+İstifadəçidən ikirəqəmli ədəd daxil etməsini xahiş edin.
+Bu rəqəmi üçrəqəmli olana kimi 7 - ədədi qədər artırın.
+Son dəyəri konsola çıxarın.
+
+Task-02
+Konsola N dəfə «I know how to use cycles» mesajı çıxaran proqram yazın.
+Proqram N ədədini istifadəçidən soruşur.
+
+Task-03
+Sonu 0 ilə bitən bütün üçrəqəmli ədədləri konsola çıxaran proqram yazın.
+
+Task-04
+Bütün ikirəqəmli tək ədədlərin cəmini konsola çıxaran proqram yazın.
+
+Task-05
+İstifadəçinin daxil etdiyi ədədin bölünəni olan bütün üçrəqəmli ədədlərin cəmini hesablayan proqram yazın.
+Proqram istifadəçidən ədəd daxil etməyi xahiş edir. Bundan sonra proqram konsolda bu rəqəmin
+bölünəni olan bütün üçrəqəmli ədədlərin cəmini çıxarır.*/
+
+/*Task-06
+Sadə ədədləri çıxaran proqram yazın.
+İstifadəçidən n ədədini soruşan və n-dən 0-a kimi bütün tek ədədləri azalan ardıcıllıqla çıxaran proqram yazın.
+«16» ədədi daxil edildikdə, konsola «15», «13», «11», «9», «7», «5», «3», «1» ardıcıllığı çıxarılmalıdır.*/
+/*let a=+prompt("eded daxil et")
+for(let i=1 ;i<=a;i++2)
+
+
+/*Task-07-dən Task-10-a qədər for ilə yazılmalıdır!
+Task-07
+10-a qədər bütün ədədlərin hasilin tapan proqram tərtib edin
+For dövrün köməyi ilə 10-a qədər bütün ədədlərin hasilin tapan proqram tərtib edin.
+Alınan dəyəri konsola çıxarın.*/
+
+/*Alınan dəyəri konsola çıxarın.*/
